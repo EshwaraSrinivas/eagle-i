e87875a7f9b4fc8df60dcc5f4163e7628df28525
EshwaraSrinivas <eswarasrinivaslanka@gmail.com>
2018-11-29 12:40:52 +0530

    added check status call

--- src/components/apps/AppItem.js
+++ src/components/apps/AppItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const appList = ({app}) => {
-    return (
-        <div className="item">
-            <i className={`large ${app.appname} middle aligned icon`}></i>
-            <div className="content">
-                <a className="header" href={app.appurl}>{app.appname}</a>
-                <div className="description">Updated 10 mins ago</div>
+class appList extends React.Component {
+    render() {
+        return (
+            <div className="item">
+                <i className={`large ${this.props.app.appname} middle aligned icon`}></i>
+                <div className="content">
+                    <a className="header" href={this.props.app.appurl}>{this.props.app.appname}</a>
+                    <div className="description">Updated 10 mins ago</div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default appList;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Home.css";
 import AppList from '../apps/AppsList';
+import Axios from 'axios';
 
 class Home extends React.Component {
+
+    checkStatus(){
+        Axios.get('https://api.github.com/users/maecapozzi').then((response) => {
+            console.log(response);
+        })
+    }
     render() {
         return (
             <div className="applications">
                 <div className="test-status">
-                    <button className="ui button button-status">Check Status</button>
+                    <button className="ui button button-status" onClick={this.checkStatus}>Check Status</button>
                 </div>
                 <AppList />
             </div>
