da2dc9349c24de9d0d3039aba6572f815e242042
EshwaraSrinivas <eswarasrinivaslanka@gmail.com>
2018-11-29 12:45:14 +0530

    updated build

---
+++ precache-manifest.1d1f5168653747838428ac75cf95ff89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "04f2195cf737bb68523c90ccb16fcf9f",
+    "url": "/static/media/logo.04f2195c.png"
+  },
+  {
+    "revision": "6528324393215c663f9c05dd5b3497b1",
+    "url": "/static/media/backlit-bird-clouds-755385.65283243.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4f9dbc3f0c8b96b47f8",
+    "url": "/static/js/main.b4f9dbc3.chunk.js"
+  },
+  {
+    "revision": "29a300d9c2ed1d3a6a34",
+    "url": "/static/js/1.29a300d9.chunk.js"
+  },
+  {
+    "revision": "b4f9dbc3f0c8b96b47f8",
+    "url": "/static/css/main.40f075b8.chunk.css"
+  },
+  {
+    "revision": "ecfdbaaa5dacf358f94e908c92d84454",
+    "url": "/index.html"
+  }
+];
